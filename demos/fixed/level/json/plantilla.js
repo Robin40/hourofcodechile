1a475e7fc57ffa2774b2351eeabdd479e3bdd7c9
Robin40 <r.a.c.f.28@gmail.com>
2015-09-26 19:40:50 -0300

    plantilla + tileSimbolo
    lucas mira esto

---
+++ demos/fixed/level/json/plantilla.js
@@ -0,0 +1,43 @@
+var HOC_LEVEL = {
+	"nombre": "Nombre del nivel",
+	"descripcion": "Descripción del nivel",
+	"grid": {
+		"filas": r,
+		"columnas": c,
+		"matriz": [
+			"XYXYXYXYXYXY",
+			"YXYXYXYXYXYX",
+			"XYXYXYRYXYXY",
+			"YXYXYR.RYXYX",
+			"XYRRRR.RRYXY",
+			"YR.....RRRYX",
+			"XYRR.RRRRYXY",
+			"YXYXYXYXYXYX",
+			"XYXYXYXYXYXY",
+			"YXYXYXYXYXYX",
+			"XYXYXYXYXYXY",
+			"YXYXYXYXYXYX"
+		]
+	},
+	"personaje": {
+		"tipo": "mariohugo",
+		"fila": indexada desde 0,
+		"columna": indexada desde 0,
+		"orientacion": "arriba"/"izquierda"/"derecha"/"abajo"
+	},
+	"meta": {
+		"tipo": "perro",
+		"fila": indexada desde 0,
+		"columna": indexada desde 0,
+		"orientacion": "arriba"/"izquierda"/"derecha"/"abajo"
+	}
+	"cacas": {
+		"cantidad": n
+		"requeridas": k
+		"c": [
+			{"fila": i, "columna": j},
+			{"fila": i, "columna": j},
+			...
+		]
+	}
+};
---
+++ demos/fixed/level/json/tileSimbolo.js
@@ -0,0 +1,7 @@
+var tileSimbolo = {
+	"X": "pasto_oscuro",
+	"Y": "pasto_claro",
+	".": "tierra",
+	"R": "bloque_muralla",
+	"?": "missing_tile"
+}
