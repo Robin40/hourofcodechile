04bc1b12d5124fc35e9dec0492f3ff0b11fa8cc8
LucasCabello <lmcabellog@gmail.com>
2015-09-27 05:16:35 -0300

    Level 8

--- demos/fixed/level/json/level_8.js
+++ demos/fixed/level/json/level_8.js
@@ -1,26 +1,36 @@
 var HOC_LEVEL = {
 	"nombre": "Nombre del nivel",
 	"descripcion": "Descripción del nivel",
+	"maximoBloques": 5,
 	"grid": {
 		"filas": 10,
 		"columnas": 10,
-		"matriz": [
-		"XXXXXXXXXX",
-		"XXXXXXXXXX",
-		"qRRRRRRRRw",
-		"T........G",
-		"T........G",
-		"T........G",
-		"T........G",
-		"aFFFFFFFFs",
-		"XXXXXXXXXX",
-		"XXXXXXXXXX"
-		]
+		"matriz": 
+		   ["qRRRRRRRRw",
+		   	"T........G",
+			"T.qFFFFw.G",
+			"T.GXXXXT.G",
+			"T.GXXXXT.G",
+			"T.GXXXXT.G",
+			"T.GXXXXT.G",
+			"T.GXXXXT.G",
+			"T.GXXXXT.G",
+			"aFsXXXXaFs"]
 	},
 	"personaje": {
 		"tipo": "mariohugo",
-		"fila": 5,
+		"fila": 8,
 		"columna": 1,
-		"orientacion": "derecha"
+		"orientacion": "arriba"
+	},
+	"metas": {
+		"cantidad": 1,
+		"m": [
+			{
+				"tipo": "perro",
+				"fila": 8, "columna": 8,
+				"orientacion":"izquierda"
+			}
+		]
 	}
 };
