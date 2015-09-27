bb2286c9b33ad61f1dc911fc3008fc8637eff42a
Mauricio Quezada <waxkun@gmail.com>
2015-09-27 18:04:54 -0300

    fix del diseño en disp moviles

--- demos/fixed/include/head.php
+++ demos/fixed/include/head.php
@@ -1,5 +1,6 @@
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">      
   <title>Blockly Demo: Fixed Blockly</title>
   
   <?php include_once __DIR__.'/CSS.php'; ?>
