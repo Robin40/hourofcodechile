8a87984595d2a122207732257d5e7d9a09daf3d1
LucasCabello <lmcabellog@gmail.com>
2015-09-27 22:50:03 -0300

    bloques L16

---
+++ demos/fixed/level/block/block_16.php
@@ -0,0 +1,29 @@
+<xml id="toolbox" style="display: none">
+    <block type="avanzar"></block>
+  <block type="girar">
+    <field name="sentido">izquierda</field>
+  </block>
+  <block type="girar">
+    <field name="sentido">derecha</field>
+  </block>
+    <block type="repetir_hasta"></block>
+    <block type="if_caca"></block>
+  </xml>
+
+  <xml id="startBlocks" style="display: none">
+  <block type="al_ejecutar" x="32" y="32">
+    <next>
+      <block type="repetir_hasta">
+        <statement name ="until_body">
+          <block type="if_caca">
+            <field name="sentido">derecha</field>
+            <next>
+              <block type="avanzar"></block>
+          </next>
+          </block>
+        </statement>
+      </block>
+    </next>
+  </block>
+  </xml>
+
