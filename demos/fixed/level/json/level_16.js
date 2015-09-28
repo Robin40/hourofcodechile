1bb28732ad48d18e15dce5c7b768b4932346efda
LucasCabello <lmcabellog@gmail.com>
2015-09-28 02:45:29 -0300

    descripcion L16

--- demos/fixed/level/json/level_16.js
+++ demos/fixed/level/json/level_16.js
@@ -1,6 +1,17 @@
 var HOC_LEVEL = {
-	"nombre": "Nombre del nivel",
-	"descripcion": "Descripción del nivel",
+	"nombre": "",
+	"imagen_inicial": "caca.png",
+	"maximoBloques": 4,
+	"bloquesRequeridos": {
+		"cantidad": 1,
+		"b": [
+			{"tipo": "repetir_hasta", "cantidad": 1}
+		]
+	}
+	"titulo_ayuda": "Sólo debes girar.",
+	"comentario_ayuda": "Para completar el código, sólo debes girar. Mira la condición del bloque \"si\", para decidir en qué dirección debes girar.",
+	"mensaje_inicial": "Completa el código para poder recoger la caca.",
+	"descripcion": "Completa el código para poder recoger la caca.",
 	"grid": {
 		"filas": 10,
 		"columnas": 10,
