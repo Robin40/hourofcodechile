5551d55bd58dde8821f51210ea9cc392fe39a7d5
LucasCabello <lmcabellog@gmail.com>
2015-09-28 03:12:53 -0300

    descripciones de niveles terminados

--- demos/fixed/level/json/level_20.js
+++ demos/fixed/level/json/level_20.js
@@ -1,7 +1,11 @@
 var HOC_LEVEL = {
-    "nombre": "Nombre del nivel",
-    "descripcion": "Descripción del nivel",
-    "maximoBloques": 5,
+    "nombre": "",
+    "imagen_inicial": "caca.png",
+    "titulo_ayuda": "Confía en tus conocimientos",
+    "comentario_ayuda": "Usa todo lo que has aprendido. Si lo necesitas vuelve a algún nivel anterior para recordar los bloques necesarios. ¡Buena suerte!",
+    "mensaje_inicial": "Recoge toda la caca que han dejado tus perros, pero no te intimides por el nivel, ya tienes todo el poder necesario para lograrlo.",
+    "descripcion": "Recoge toda la caca que han dejado tus perros, pero no te intimides por el nivel, ya tienes todo el poder necesario para lograrlo.",
+    "maximoBloques": 6,
     "grid": {
         "filas": 10,
         "columnas": 10,
@@ -58,5 +62,13 @@ var HOC_LEVEL = {
             {"tipo": "fuego1", "fila": 9, "columna": 8},
             {"tipo": "fuego2", "fila": 9, "columna": 9},
         ],
+    },
+    "bloquesRequeridos": {
+        "cantidad": 5,
+        "b": [
+            {"tipo": "repetir_hasta", "cantidad": 1},
+            {"tipo": "if_caca", "cantidad": 2},
+            {"tipo": "girar", "cantidad": 2}
+        ]
     }
 };
