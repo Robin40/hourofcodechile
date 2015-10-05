244d21ba4b3735121a6e88c2dc55c667ba151169
Mauricio Quezada <waxkun@gmail.com>
2015-10-05 13:06:26 -0300

    cambios en pagina final

--- demos/fixed/felicitaciones.php
+++ demos/fixed/felicitaciones.php
@@ -17,6 +17,8 @@
     }
 
     $("body").css("background-color", getRandomColor());
+    $("#ahora").css("background-color", getRandomColor());
+    $("#peticion").css("background-color", getRandomColor());
     
     success = '<span id="success1" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span><span id="inputSuccess2Status" class="sr-only">(success)</span>';
     error = '<span id="error1" class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span><span id="inputError2Status" class="sr-only">(error)</span>';
@@ -63,44 +65,54 @@
     body { color: white; background-color: #00ca9b }
     h1, h2, h3, h4, h5 { text-shadow: 2px 2px gray; }
     p { text-shadow: 1px 1px gray; font-size: 20px }
+    #ahora, #peticion { height: 250px; border-radius: 5px;}
+    .pt { padding-top: 30px }
+    .container-fluid { padding-bottom: 30px; }
   </style>
   </head> 
   <body>
     <div class="container-fluid">
       <div class="row">
         <div class="col-xs-12 text-center" style="padding-top: 20px;">
-          <img src="media/hoc-logo-medium.png" />
+          <img src="media/hoc-logo-medium.png" width="120" />
           <h1>&iexcl; Felicitaciones !</h1>
-  	  <p>&iexcl;Has superado el nivel 20 de la Hora del
-  	    C&oacute;digo!</p>
+  	  <p>&iexcl;Has completado una Hora del C&oacute;digo!</p>
 
+          <!-- Te contamos un secreto, hay niveles bonus escondidos en -->
+          <!-- el juego, para eso, solo debes avanzar al nivel 21 :-). ¡Suerte! -->
 
-          <!-- <p>Te contamos un secreto, hay niveles bonus escondidos en -->
-          <!-- el juego, para eso, solo debes avanzar al nivel 21 :-). ¡Suerte!</p> -->
-          
           <div class="col-xs-12" style="padding-top: 20px">
-            <a class="btn btn-warning btn-lg" href="http://c-100.cl/recursos/"
-               target="_blank">¿Qu&eacute; pasa despu&eacute;s de la Hora
-              del C&oacute;digo?</a>
+            <img src="media/diploma.png" width="150" />
           </div>
-            
-          <div class="col-xs-12" style="padding-top: 20px;">
+          
+          <div class="col-xs-12" style="padding-top: 20px;
+                                        padding-bottom: 30px;">
             <p>Obt&eacute;n tu diploma</p>
           
-            <form class="form-inline" id="form-diploma" method="post" action="crear_diploma.php">
+            <form target="_blank" class="form-inline" id="form-diploma" method="post" action="crear_diploma.php">
               <div class="form-group has-feedback" id="nombreDiv">
                 <input type="text" class="form-control" id="nombre" name="nombre"
                        aria-describedby="inputSuccess2Status"
                        placeholder="Ingresa tu nombre">
               </div>
-
               <button id="submit" type="submit" class="btn btn-primary">&iexcl;Crear Diploma!</button>
             </form>
           </div>
 
-          <div class="col-xs-12" style="padding-top: 20px">
-            <img src="media/diploma.png" width="150" />
+
+          <div class="col-xs-3"></div>
+          <div id="ahora" class="col-xs-3 pt" style="margin-right: 5px">
+            <a class="btn btn-warning btn-lg" href="http://c-100.cl/recursos/"
+               target="_blank">¿Qu&eacute; pasa ahora?</a>
+            <p style="padding-top: 10px">¡Sigue aprendiendo o ense&ntilde;ando con estos recursos!</p>
           </div>
+          <div id="peticion" class="col-xs-3 pt" style="margin-left: 5px">
+            <a class="btn btn-success btn-lg" href="https://www.change.org/p/ministerio-de-educación-de-chile-quiero-que-todos-los-estudiantes-de-chile-puedan-aprendan-programación-computacional"
+               target="_blank">Firma nuestra petición</a>
+            <p style="padding-top: 10px">¡Para ense&ntilde;ar Programaci&oacute;n Computacional
+            en todas las escuelas de Chile!</p>
+          </div>
+          <div class="col-xs-3"></div>
         </div>
       </div>
     </div>
